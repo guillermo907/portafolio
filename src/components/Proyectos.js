7cf9b7a8f35c6d29b628caa1c9f3810e5015d316
Guillermo Lòpez Garcìa <willy@MacBook-Pro-de-Guillermo.local>
2020-12-06 01:32:28 -0600

    cambios

--- src/components/Proyectos.js
+++ src/components/Proyectos.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import csgImage from '../csg.png';
+import ecideasImage from '../ecideas.png';
+import famImage from '../famdefe2.png';
+
 const ProyectosContainer = styled.div`
+padding: 30px;
     display:flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     background-color: var(--blue-color);
     min-height: 70vh;
+
+    .projects{
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: space-evenly;
+        div{
+            margin: 20px;
+        }
+    }
+
+    img {
+        max-width: 360px;
+        max-height: 228px;
+        border-radius: 10px;
+        &:hover{
+            filter: invert(90%);
+            cursor: pointer;
+        }
+    }
 `;
 
 const Proyectos = () =>{
@@ -14,6 +40,23 @@ const Proyectos = () =>{
     return(
         <ProyectosContainer>
             Algunos proyectos
+            <div style = {{maxWidth: "100%"}}>
+                <div class="projects">
+                    <div>
+                        <h1>Confianza Solidaria</h1>
+                        <img src={csgImage}/>
+                    </div>
+                    <div>
+                        <h1>ECIDEAS</h1>
+                        <img src={ecideasImage}/>
+                    </div>
+                    <div>
+                        <h1>Familias de Fe</h1>
+                        <img src={famImage}/>
+                    </div>
+                    
+                </div>
+            </div>
         </ProyectosContainer>
     );
 }
